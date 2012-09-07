d2d0985d81a88c848c3293e47a3a39827e498b00
Guenther Brunthaler <root@xrasp.emgenxx69lwyn5ctlr4nl64ul.local>
2012-09-07 07:04:28 +0200

    Add debian-specific sbin directory only to superuser's path

--- profile.d/50xworld_sbin_debian.sh
+++ profile.d/50xworld_sbin_debian.sh
@@ -1,3 +1,6 @@
 #! /bin/false
-. /usr/local/libexec/xworld/sh/path_hyec3v5m8kd1vjs8k7d1wce62.sh \
-	--prepend /usr/local/sbin/xworld_debian
+if test x`id -u` = x0
+then
+	. /usr/local/libexec/xworld/sh/path_hyec3v5m8kd1vjs8k7d1wce62.sh \
+		--prepend /usr/local/sbin/xworld_debian
+fi
